44c88953dce4ebb9e06aa6fe0f20d8dcf9090bb0
LesleyBonyo <lesleybonyo@gmail.com>
2024-02-26 16:36:04 +0300

    PHP Syntax

---
+++ Group A/26-02-2024a/introduction.php
@@ -0,0 +1,79 @@
+<?php //opening php tag
+# single line comment
+/* Multiple line 
+comment */
+echo "Hello World";
+$age = 17; // integer
+$width = 14.2; //float
+$first_name = "John"; //string
+$present = false; //boolean
+$unit = null; //null
+
+echo "<br>";
+echo var_dump($age)."<br>";
+echo var_dump($width)."<br>";
+echo var_dump($first_name)."<br>";
+echo var_dump($present)."<br>";
+echo var_dump($unit)."<br>";
+
+//arrays - array()
+//indexed array
+$student_names = array('Joan', 'Abel', 'Clara');
+echo "Second student is $student_names[1]";
+// associative arrays
+$student_heights = array(
+	'Joan'=> 12.5,
+	'Abel'=> 10.2,
+	'Clara'=> 5.7
+);
+echo $student_heights['Clara'];
+//multidimensional arrays
+$months_days = array(
+	array('Jan', 'Feb', 'Mar'),
+	array(1,2,3)
+);
+echo $months_days[0][2];
+
+// loop through indexed arrays
+//foreach
+foreach($student_names as $name){
+	echo "<div>Student name is $name </div>";
+}
+// loop through associative arrays
+foreach($student_heights as $students => $height){
+	echo "<div>$students is $height cm</div>";
+}
+//for loop
+for ($i=1; $i < 3 ; $i++) { 
+	echo "<h1>Chapter $i</h1>";
+	echo "<button>Hello $i </button>";
+}
+$x = 1;
+while ($x < 4) {
+	echo "<div>Hello $x</div>";
+	$x++;
+}
+// conditional statements - if else
+$y = 22;
+if ($y < 18) {
+	echo "Too young";
+} elseif ($y == 18) {
+	echo "Perfect";
+} else {
+	echo "Too old";
+}
+// functions
+function first_function(){
+	echo "<h3> Hello Function </h3>";
+}
+first_function();
+
+function get_product($y, $z){
+	return $y * $z;
+}
+echo get_product(14,5);
+
+
+
+
+//closing tag?>
---
+++ Group B/26-02-2024/introduction.php
@@ -0,0 +1,79 @@
+<?php // opening tag
+# single line comment
+/* multiple line comment */
+echo "Hello World";
+// data types
+$age = 15; //integer
+$height = 12.5; //float
+$first_name = "Clara"; //string
+$register = false; //boolean
+$school = null; // null
+
+echo "<br>";
+echo var_dump($age)."<br>";
+echo var_dump($height)."<br>";
+echo var_dump($first_name)."<br>";
+echo var_dump($register)."<br>";
+echo var_dump($school)."<br>";
+
+//arrays - array()
+//indexed arrays
+$students = array('John', 'Jane', 'Mark');
+echo "<br>Third student is $students[2]";
+// associative arrays
+$student_ages = array(
+	'John'=> 12,
+	'Jane'=> 14,
+	'Mark'=> 16
+	);
+echo $student_ages['Jane'];
+//multidimensional arrays
+$years_months = array(
+	array(2020, 2021, 2022),
+	array("Jan", "Feb", "Mar")
+);
+echo "<br>";
+echo $years_months[1][1];
+// loop through indexed arrays - foreach
+foreach ($students as $student) {
+	echo "Student name $student <br>";
+}
+// loop through associative array
+foreach($student_ages as $student => $age){
+	echo "$student is $age <br>";
+}
+// for loop
+for ($i=1; $i < 3 ; $i++) { echo "<h1> This is a heading</h1>";
+	echo "<button> hello</button>";
+	// code...
+}
+// while loop
+$x = 1;
+while ($x < 4) {
+	echo "<h3>Chapter $x</h3>";
+	$x++;
+}
+//conditional statements
+//if else
+$y = 20;
+if ($y < 15) {
+	echo "Too low";
+} elseif ($y == 15) {
+	echo "Exactly";
+} else{
+	echo "Too high";
+}
+// function
+function first_function(){
+	echo "<h6>First Function</h6>";
+}
+first_function();
+
+function get_product($y, $x){
+	return $y * $x;
+}
+echo get_product(10, 13);
+
+
+
+//closing tag ?>
